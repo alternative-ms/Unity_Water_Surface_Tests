5d558064eff55b148c3d0e91f69a20a57d2b90fc
Alexander Tkachenko <alt@alternative.ms>
2026-06-26 13:09:41 +0300

    Level1 basic shader test and example meshes

---
+++ Assets/Water_Surface_Tests_Build-In/Level1_BasicShaderMods/Level1_Pearlescent_Chameleon.shader
@@ -0,0 +1,58 @@
+Shader "Custom/Level1_Pearlescent_Chameleon"
+{
+    Properties
+    {
+        _MainColor ("Base Color (Center)", Color) = (0.5, 0.0, 0.5, 1.0)
+        _ChameleonColor ("Chameleon Color (Fresnel)", Color) = (0.0, 1.0, 1.0, 1.0)
+        _FresnelPower ("Fresnel Power", Range(0.001, 8.0)) = 1.0
+        _FresnelMultipler ("Fresnel Multipler", Range(0, 4)) = 1.0
+        _Cube ("Custom Cubemap", Cube) = "_Skybox" {}
+        _ReflectionIntensity ("Reflection Intensity", Range(0.0, 2.0)) = 1.0
+        _Roughness ("Roughness", Range(0.0, 1.0)) = 0.2
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+            #pragma surface surf Standard fullforwardshadows
+            #pragma fragmentoption ARB_precision_hint_fastest
+        
+            #pragma target 3.5
+
+            struct Input
+            {
+                float3 viewDir;
+                float3 worldRefl;
+            };
+
+            fixed4 _MainColor;
+            fixed4 _ChameleonColor;
+            half _FresnelPower;
+            half _FresnelMultipler;
+
+            UNITY_DECLARE_TEXCUBE(_Cube);
+        
+            half _ReflectionIntensity;
+            half _Roughness;
+
+            void surf (Input IN, inout SurfaceOutputStandard o)
+            {
+                half fresnel = 1.0 - saturate(dot(normalize(IN.viewDir), o.Normal));
+                half colorFresnel = pow(fresnel, _FresnelPower) * _FresnelMultipler;
+                fixed4 finalColor = lerp(_MainColor, _ChameleonColor, colorFresnel);
+                o.Albedo = finalColor.rgb;
+                half mipLevel = _Roughness * 8.0; 
+                fixed4 reflection = UNITY_SAMPLE_TEXCUBE_LOD(_Cube, IN.worldRefl, mipLevel);
+                half reflectionFresnel = lerp(0.2, 1.0, fresnel); 
+                o.Emission = reflection.rgb * _ReflectionIntensity * reflectionFresnel; 
+                o.Metallic = 0.0;
+                o.Smoothness = 1.0 - _Roughness; 
+                o.Alpha = finalColor.a;
+            }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Water_Surface_Tests_Build-In/Scripts/SimpleCameraOrbit.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class SimpleCameraOrbit : MonoBehaviour
+{
+    [SerializeField] private Transform orbitTarget;
+    [SerializeField] private float lerpSpeed = 3f;
+    [SerializeField] private float scrollSpeed = 5f;
+    [SerializeField] private float panSpeed = 2f;
+    [SerializeField] private float minDist = 0.5f;
+    [SerializeField] private float maxDist = 5f;
+    [SerializeField] private float eulerMin = 15f;
+    [SerializeField] private float eulerMax = 75f;
+    [SerializeField] private float panYMod = 1f;
+
+    private float wDistance;
+    private float cDistance;
+    private Vector3 wEuler;
+    private Vector3 cEuler;
+    private Vector3 wOrigin;
+    private Vector3 cOrigin;
+    private bool isPanning = false;
+
+    void Start()
+    {
+        cEuler = transform.localEulerAngles;
+        wEuler = cEuler;
+
+        if (orbitTarget != null)
+        {
+            wOrigin = orbitTarget.position;
+            cOrigin = wOrigin;
+        }
+        else
+        {
+            wOrigin = new Vector3(0, panYMod, 0);//Vector3.zero;
+            cOrigin = wOrigin;//Vector3.zero;
+        }
+
+        wDistance = Vector3.Distance(cOrigin, transform.position);
+
+        wDistance = Mathf.Clamp(wDistance, minDist, maxDist);
+        cDistance = wDistance;
+
+        transform.position = cOrigin - transform.forward * cDistance;
+    }
+
+    void Update()
+    {
+        float _lerpTime = Time.deltaTime * 2f * lerpSpeed;
+
+        wDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 5f * scrollSpeed;
+        wDistance = Mathf.Clamp(wDistance, minDist, maxDist);
+        cDistance = Mathf.Lerp(cDistance, wDistance, _lerpTime);
+
+        if (Input.GetMouseButton(1))
+        {
+            wEuler.y += Input.GetAxis("Mouse X") * 3f;
+            wEuler.x -= Input.GetAxis("Mouse Y") * 3f;
+
+            wEuler.x = Mathf.Clamp(wEuler.x, eulerMin, eulerMax);
+
+            wEuler.y %= 360f;
+            if (wEuler.y < 0f)
+            {
+                wEuler.y += 360f;
+            }
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            isPanning = true;
+
+            float moveX = -Input.GetAxis("Mouse X") * panSpeed * (cDistance * 0.5f) * Time.deltaTime;
+            float moveY = -Input.GetAxis("Mouse Y") * panSpeed * (cDistance * 0.5f) * Time.deltaTime;
+
+            wOrigin += transform.right * moveX + transform.up * moveY;
+        }
+
+        wEuler.z = 0f;
+
+        cEuler.x = Mathf.LerpAngle(cEuler.x, wEuler.x, _lerpTime);
+        cEuler.y = Mathf.LerpAngle(cEuler.y, wEuler.y, _lerpTime);
+        cEuler.z = Mathf.LerpAngle(cEuler.z, 0f, _lerpTime);
+
+        transform.rotation = Quaternion.Euler(cEuler);
+
+        if (!isPanning)
+        {
+            if (orbitTarget != null) wOrigin = orbitTarget.position;
+            else wOrigin = new Vector3(0, panYMod, 0); //Vector3.zero;
+        }
+
+        cOrigin = Vector3.Lerp(cOrigin, wOrigin, _lerpTime);
+
+        transform.position = cOrigin - transform.forward * cDistance;
+    }
+}
