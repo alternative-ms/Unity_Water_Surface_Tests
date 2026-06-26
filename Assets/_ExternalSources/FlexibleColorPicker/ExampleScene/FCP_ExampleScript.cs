e9cca264002bb1c9c4c4aeca6cd4ca13029b6048
Alexander Tkachenko <alt@alternative.ms>
2026-06-26 19:03:37 +0300

    Update FCP_ExampleScript.cs

--- Assets/_ExternalSources/FlexibleColorPicker/ExampleScene/FCP_ExampleScript.cs
+++ Assets/_ExternalSources/FlexibleColorPicker/ExampleScene/FCP_ExampleScript.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 
-public class FCP_ExampleScript : MonoBehaviour {
+public class FCP_ExampleScript : MonoBehaviour
+{
 
     public bool getStartingColorFromMaterial;
     public FlexibleColorPicker fcp;
     public Material material;
 
-    private void Start() {
-        if(getStartingColorFromMaterial)
-            fcp.color = material.color;
+    public string colorName = "_Color"; // Added
+
+    private void Start()
+    {
+        if (getStartingColorFromMaterial)
+            fcp.color = material.GetColor(colorName);
 
         fcp.onColorChange.AddListener(OnChangeColor);
     }
 
-    private void OnChangeColor(Color co) {
-        material.color = co;
+    private void OnChangeColor(Color co)
+    {
+        material.SetColor(colorName, co);
     }
 }
